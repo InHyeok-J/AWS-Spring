5b31ee92adceaa7c16860d619ec969e8f9f3f916
inhyeok <ss112d86@gmail.com>
2022-03-10 19:03:06 +0900

    3장 스프링부트 JPA

---
+++ src/main/java/com/aws/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.aws.springboot.web;
+
+import com.aws.springboot.domain.posts.PostsService;
+import com.aws.springboot.web.dto.PostsResponseDto;
+import com.aws.springboot.web.dto.PostsSaveRequestDto;
+import com.aws.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long Save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+}
