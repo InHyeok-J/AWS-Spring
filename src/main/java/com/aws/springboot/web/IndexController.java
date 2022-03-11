e968780033bbbe1df418873086be68104fb09f90
inhyeok <ss112d86@gmail.com>
2022-03-12 04:41:08 +0900

    5장 스프링 시큐리티와 OAuth2.0 완료

--- src/main/java/com/aws/springboot/web/IndexController.java
+++ src/main/java/com/aws/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.aws.springboot.web;
 
+import com.aws.springboot.config.auth.LoginUser;
+import com.aws.springboot.config.auth.dto.SessionUser;
 import com.aws.springboot.domain.posts.PostsService;
 import com.aws.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user !=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
